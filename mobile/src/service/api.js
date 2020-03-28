7133e328414c012820b01c315d8fa4ae40235403
albino29 <>
2020-03-28 14:12:07 -0300

    changing splash, pc ip, logo

--- mobile/src/service/api.js
+++ mobile/src/service/api.js
@@ -1,6 +1,6 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://192.168.1.25:3333'
+  baseURL: "http://192.168.1.2:3333"
 });
 export default api;
